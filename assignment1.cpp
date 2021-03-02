00588c4e9e1fdff4b322bb1b599de056bb4dacba
KLAU-S <75158477+KLAU-S@users.noreply.github.com>
2021-03-02 20:52:40 +0530

    Update assignment1.cpp

--- assignment1.cpp
+++ assignment1.cpp
@@ -7,57 +7,31 @@ using namespace std;
 int i,j,r,c;
 int matrix[100][100];
 void inpmatrix(int r,int c){
-
     for(i=0;i<r;i++){
-
         for(j=0;j<c;j++){
-
             cin>>matrix[i][j];
-
         }
-
     }
-
 }
 void outmatrix(int r,int c){
-
     for(i=0;i<r;i++){
-
         for(j=0;j<c;j++){
-
             cout<<matrix[i][j]<<" ";
-
         }
-
         cout<<" \n";
-
     }
-
 }
 int main(){
-
     cout<<"Enter the size of matrix. \n";
-
     cout<<"Enter the size of row. \n";
-
     cin>>r;
-
     cout<<"Enter the size of colom. \n";
-
     cin>>c;
-
     cout<<"Enter the data. ";
-
     inpmatrix(r,c);
-
-
     cout<<"Now data is displayed below. \n";
-
-
-    outmatrix(r,c);
-
+    outmatrix(r,c);
     return 0;
-
 }
 
 
